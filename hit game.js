8c688ec9c6587bba530a77352e311f949565d0d1
shiv-am117 <goel.shivam117@gmail.com>
2018-06-28 23:36:41 +0530

    loop removed

--- hit game.js
+++ hit game.js
@@ -2,28 +2,27 @@ $(document).ready(function(){
 	var score=0;
 	$("#score").text("Score :  "+score);
 	var count=1;
-	var time=2000;
-	var ftime=1000;
-	function algo(){
-		flag=0;
-		var rand=(Math.floor(Math.random()*10))%9+1;
-		var idreq='b'+rand;
-		if(count>0) {text="X"; count*=-1; time=1000;}
-		else {text="O"; count*=-1; time=ftime--;}
-		for (var i = 1 ; i <= 9; i++) {
-			var curid='b'+i;
-			if(curid===idreq) $("#"+curid).text(text);
-			else $("#"+curid).text("O");
-			
-		}
-		$("#"+idreq).click(function(){
-			if($("#"+idreq).text()==="X") {score+=100; $("#"+idreq).text("O"); }
-			$("#score").text("Score :  "+score);
-		});
 
+	function algo(){
+	
+		if(count>0) {
+		rand=(Math.floor(Math.random()*10))%9+1;
+	    idreq='b'+rand;
+		text="X"; count*=-1;
+		}
+		else {text="O"; count*=-1; }
+		 $("#"+idreq).text(text);
+			
+			
+		
+		$("#"+idreq).click(function(){
+			if($(this).text()==="X") {score+=100; $("#"+idreq).text("O"); }
+			$("#score").text("Score :  "+score);
+
+		});
 		
 	}
+	setInterval(algo,1000);
 
-	setInterval(algo,time);
 
 });
